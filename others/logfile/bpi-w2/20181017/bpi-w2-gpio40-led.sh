e90ba54ed24153fdd7d7f8d313e1195e79b897a1
Banana Pi -BPI <lionwang@sinovoip.com.cn>
2018-10-17 10:33:24 +0800

    logfile: add bpi-w2-gpio40-led.sh for gpio40 & led test

---
+++ others/logfile/bpi-w2/20181017/bpi-w2-gpio40-led.sh
@@ -0,0 +1,59 @@
+#bin/bash
+#GPIO40_NO_IGPIO="13 14 3 17 58 25 27 7 4 5 6 8 12 11 100 21 54 59 57 22 23 24 60"
+#IGPIOSORG="21 3 2 5 4 9"
+#IGPIO + 101 = GPIO
+#IGPIOS="122  104 103 106 105 110"
+GPIO40="13 14 122 104 103 3 17 58 25 27 106 105 7 4 110 5 6 8 12 11 100 21 54 59 57 22 23 24 60"
+GPIOLED="128 97 98" 
+
+GPIOS="$GPIO40 $GPIOLED"
+
+export_gpio() {
+  for IN in $GPIOS ; do
+    EXPORT=/sys/class/gpio/export
+    VALUE=/sys/class/gpio/gpio$IN/value
+    if [ ! -f $VALUE ] ; then
+      echo $IN > $EXPORT
+    fi
+  done
+}
+
+out_gpio() {
+  for IN in $GPIOS ; do
+    DIR=/sys/class/gpio/gpio$IN/direction
+    if [ -f $DIR ] ; then
+      echo  out > $DIR
+    fi
+  done
+}
+
+hi_gpio() {
+  for IN in $GPIOS ; do
+    VALUE=/sys/class/gpio/gpio$IN/value
+    if [ -f $VALUE ] ; then
+      echo 1 > $VALUE
+    fi
+  done
+}
+
+on_off_led()                                                                    
+{                                                                               
+ONOFF=$1                                                                        
+  for IN in $GPIOS ; do
+    VALUE=/sys/class/gpio/gpio$IN/value
+    if [ -f $VALUE ] ; then
+      echo $ONOFF > $VALUE
+    fi
+  done
+}                                                                               
+                                                                                
+export_gpio
+out_gpio
+hi_gpio
+
+while [ 1 ] ; do
+  on_off_led 0                                                            
+  sleep .5                                                                
+  on_off_led 1                                                            
+  sleep .5                                                                
+done
