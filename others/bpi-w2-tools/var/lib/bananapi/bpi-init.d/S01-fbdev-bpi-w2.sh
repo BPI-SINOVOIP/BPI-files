49c70c5638b075419cf78e6f5f9d39102e54915b
Banana Pi -BPI <lionwang@sinovoip.com.cn>
2019-09-25 22:32:42 +0800

    bpi-w2-tools.tgz: add bpi-w2-daemon-4.4 for kernel 4.4

--- others/bpi-w2-tools/var/lib/bananapi/bpi-init.d/S01-fbdev-bpi-w2.sh
+++ others/bpi-w2-tools/var/lib/bananapi/bpi-init.d/S01-fbdev-bpi-w2.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 BOARD=$(bpi-hw)
+service=linux
+
+check_cmdline()
+{
+  # parse command line arguments
+  for arg in $(cat /proc/cmdline); do
+    case $arg in
+      service=*)
+        service="${arg#*=}"
+        return
+        ;;
+    esac
+  done
+}
 
 fbdev-bpi-w2() {
   if [ "x$BOARD" != "xbpi-w2" ] ; then
@@ -7,16 +21,21 @@ fbdev-bpi-w2() {
       return 0
     fi
   fi
+  check_cmdline
 
   mkdir -p /dev/graphics
   ln -s /dev/fb0 /dev/graphics/fb0
 
   echo 10240 > /sys/kernel/rtk_alsa/dec_ao_buffer_size
-  /usr/local/bin/bpi-w2-daemon > /dev/null &
+  if [ "x$service" == "xlinux4.4" ] ; then
+    /usr/local/bin/bpi-w2-daemon-4.4 > /dev/null &
+  else
+    /usr/local/bin/bpi-w2-daemon > /dev/null &
+  fi
        
-  sleep 5
+  #sleep 5
   #/usr/bin/rtkInitFb 39 250
-  /usr/local/bin/bpi-w2-init 0 250
+  #/usr/local/bin/bpi-w2-init 0 250
 
 }
 
